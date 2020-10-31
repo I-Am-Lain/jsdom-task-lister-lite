86e0c3f8f44fa4ddb01881fe9702bdb15b5d4ef8
I-Am-Lain <vetter.william@gmail.com>
2020-10-30 22:10:05 -0500

    V2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const form = document.querySelector("#create-task-form")
+const list = document.querySelector("#tasks")
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+function formListener(){
+  form.addEventListener("submit", function(event){
+    event.preventDefault()
+    const input = event.target['new-task-description'].value
+
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+
+    li.innerText = input
+    button.setAttribute("data-description", `${input}`)
+    button.innerText = "X"
+    
+    li.appendChild(button)
+    list.appendChild(li)
+
+  })
+}
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+function addDelete(){
+  list.addEventListener("click", function(event){
+    if (event.target.getAttribute("data-description")) {
+      deleteTask(event)
+    }
+  })
+}
+
+function deleteTask(event){
+  const listID = event.target.dataset.description
+  const theListItem = document.querySelector(`[data-description="${listID}"`)
+  theListItem.parentNode.remove()
+}
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+function main() {
+  formListener()
+  addDelete()
+}
+
+main()
